ed872d31f5620d6b56661e4e2f5b4d59fc433bb2
Mike Liu <mike000708@me.com>
2018-10-03 20:44:48 -0400

    solved video conflict

--- src/pages/cs101.js
+++ src/pages/cs101.js
@@ -7,7 +7,7 @@ const SecondPage = () => (
   <Layout>
     <h1>Hello, students in Jingkang Zhang's Python Course</h1>
     <p>Welcome to the course home page</p>
-    <div>
+  <div>
   <h1>Lectures and Resources</h1>
   <h3>Lecture Videos</h3>
   <ul>
@@ -16,18 +16,18 @@ const SecondPage = () => (
       <li><a href="https://class.wildchickenuniversity.com/python/Lecture1.mp4">Video</a></li>
       <li><a href="https://class.wildchickenuniversity.com/python/Lecture1Notes.pdf">Notes</a></li>
       <li><a href="https://class.wildchickenuniversity.com/python/demo1.py">Lecture Codes</a></li>
-      <li><a href="https://class.wildchickenuniversity.com/python/hw1.zip">Homework</a> and <a href="https://class.wildchickenuniversity.com/python/hw1Specification.pdf">Specification</a></li><a href="https://class.wildchickenuniversity.com/python/hw1Specification.pdf">
-      </a></ul><a href="https://class.wildchickenuniversity.com/python/hw1Specification.pdf">
+      <li><a href="https://class.wildchickenuniversity.com/python/hw1.zip">Homework</a> and <a href="https://class.wildchickenuniversity.com/python/hw1Specification.pdf">Specification</a></li>
+    </ul>
       <video controls muted src="https://class.wildchickenuniversity.com/python/Lecture1.mp4" width={300} height={200}>
         Sorry, your browser doesn't support embedded videos.
       </video>
-    </a><li><a href="https://class.wildchickenuniversity.com/python/hw1Specification.pdf" /><a href="https://class.wildchickenuniversity.com/python/Lecture2.mp4">Lecture 2 Sept. 23</a></li>
+    <li><a href="https://class.wildchickenuniversity.com/python/Lecture2.mp4">Lecture 2 Sept. 23</a></li>
     <ul>
       <li><a href="https://class.wildchickenuniversity.com/python/Lecture2.mp4">Video</a></li>
       <li><a href="https://class.wildchickenuniversity.com/python/Lecture2Notes.pdf">Notes</a></li>
       <li><a href="https://class.wildchickenuniversity.com/python/demo2.py">Lecture Codes</a></li>
-      <li><a href="https://class.wildchickenuniversity.com/python/hw2.zip">Homework</a> and <a href="https://class.wildchickenuniversity.com/python/hw2Specification.pdf">Specification</a></li><a href="https://class.wildchickenuniversity.com/python/hw2Specification.pdf">
-      </a><li><a href="https://class.wildchickenuniversity.com/python/hw2Specification.pdf" /><a href="https://class.wildchickenuniversity.com/python/KeyIdeasOfRecursion.pdf">Key ideas of Recursion</a></li>
+      <li><a href="https://class.wildchickenuniversity.com/python/hw2.zip">Homework</a> and <a href="https://class.wildchickenuniversity.com/python/hw2Specification.pdf">Specification</a></li>
+      <li><a href="https://class.wildchickenuniversity.com/python/KeyIdeasOfRecursion.pdf">Key ideas of Recursion</a></li>
     </ul>
     <video controls muted src="https://class.wildchickenuniversity.com/python/Lecture2.mp4" width={300} height={200}>
       Sorry, your browser doesn't support embedded videos.
@@ -37,13 +37,13 @@ const SecondPage = () => (
       <li><a href="https://class.wildchickenuniversity.com/python/Lecture3.mp4">Video</a></li>
       <li><a href="https://class.wildchickenuniversity.com/python/Lecture3Notes.pdf">Notes</a></li>
       <li><a href="https://class.wildchickenuniversity.com/python/demo3.py">Lecture Codes</a></li>
-      <li><a href="https://class.wildchickenuniversity.com/python/hw3.zip">Homework</a> and <a href="https://class.wildchickenuniversity.com/python/hw3Specification.pdf">Specification</a></li><a href="https://class.wildchickenuniversity.com/python/hw3Specification.pdf">
-      </a></ul><a href="https://class.wildchickenuniversity.com/python/hw3Specification.pdf">
+      <li><a href="https://class.wildchickenuniversity.com/python/hw3.zip">Homework</a> and <a href="https://class.wildchickenuniversity.com/python/hw3Specification.pdf">Specification</a></li>
       <video controls muted src="https://class.wildchickenuniversity.com/python/Lecture3.mp4" width={300} height={200}>
         Sorry, your browser doesn't support embedded videos.
       </video>
-    </a></ul><a href="https://class.wildchickenuniversity.com/python/hw3Specification.pdf">
-  </a></div>
+    </ul>
+  </ul>
+  </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
